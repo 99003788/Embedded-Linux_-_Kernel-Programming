f9ec7a10af425d3a626a4636b69a9abcdd028d09
99003788 <78849542+99003788@users.noreply.github.com>
2021-04-10 14:36:56 +0530

    Update kernelthreds.c

--- Hands On/Kernel Threads/Simple Two Threads/kernelthreds.c
+++ Hands On/Kernel Threads/Simple Two Threads/kernelthreds.c
@@ -51,5 +51,5 @@ static void __exit psuedo_exit(void)
 module_init(psuedo_init);
 module_exit(psuedo_exit);
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Rajat");
+MODULE_AUTHOR("Suvradeep");
 MODULE_DESCRIPTION("A pseudo sample Module");
