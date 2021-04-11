c2552d5ddd9679da0aab459037c1b7f11c802a6f
99003788 <78849542+99003788@users.noreply.github.com>
2021-04-12 00:01:22 +0530

    Add files via upload

---
+++ Activities/System_Calls/retrieve_attribute/mysys.c
@@ -0,0 +1,12 @@
+#include <linux/syscalls.h>
+#include <linux/kernel.h>
+#include <linux/uaccess.h>
+
+SYSCALL_DEFINE0(mytestcall)
+{	struct task_struct* task_list;
+	for_each_process(task_list) 
+{
+	printk("\n %s %d \n",task_list->comm,task_list->pid,task_list->state,task_list->prio);
+	}
+	return 0;
+}
